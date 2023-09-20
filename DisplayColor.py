0b33f82f41807ba713cd017d7d4162e6a976caeb
nikhilmaheshwari01 <nikhilme1234567@gmail.com>
2023-09-21 01:48:23 +0530

    Add another program

---
+++ DisplayColor.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter their name
+name = input()
+
+# Prompt the user to enter their favorite color
+color = input()
+
+# Create a message string using the entered name and favorite color
+message = f"Your name is {name} and your favorite color is {color}."
+
+# Print the message
+print(message)
